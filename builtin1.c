14740b2954ca0aee55eda43cfe8fef7982f041dd
Psalmsage <Olonijayesamson@gmail.com>
2022-12-21 23:50:39 +0100

    simple shell project

---
+++ builtin1.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * _myhistory - displays the history list
+ * @info: structure containing potential arguments
+ * Return:Always 0.
+ */
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
+
+/**
+ * unset_allias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ * Return: Always 0 on success
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *p, c;
+	int ret;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = delete_node_at_index(&(info->alias),
+			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
+
+/**
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ * Return:Always the 0
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *p;
+
+	if (node)
+	{
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin
+ * @info: structure containing potential arguments
+ * Return: Always 0.
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
+
---
+++ string.c
@@ -0,0 +1,71 @@
+#include "shell.h"
+
+/**
+ * _strlen - return the length of a sting
+ * @s: the string whose length to check
+ * Return: integer length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (ls)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - perform lexicographoc comparison of two strings
+ * @s1: strings 1
+ * @s2: strings 2
+ * Return: negative if s1< s2 positive otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - to checks if needle starts with haystacks
+ * @haystack: string to search
+ * @needle: the substrings ton find
+ * Return: address of the next char
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source
+ * Return: pointer to destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret)
+}
